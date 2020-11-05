cbf8b296f149b3b12b28fad39c8cb974a0def2be
SynCap <syncap@ya.ru>
2020-11-06 03:03:47 +0800

    add: module incoment help text

--- Jumper.psm1
+++ Jumper.psm1
@@ -1,8 +1,103 @@
 <#
-    Jumper
-    PowerShell Console filesystem folders quick links.
-    (c)CLosk, 2020
-    https://github.com/SynCap/ps-jumper
+    .Synopsis
+
+        Jumper
+        PowerShell Console filesystem folders quick links.
+        (c)CLosk, 2020
+        https://github.com/SynCap/ps-jumper
+
+    .Description
+
+        # PS Jumper
+
+            Module to provide folders' shortcuts in PowerShell console - quick jumps by labels - the aliases of the target.
+
+        ## Features
+
+            - Links stored in JSON and/or INI files;
+            - Target of links can contain environment variables;
+            - Target of links can contain native PowerShell expressions evaluated at call time;
+            - Lists of links can be combined from several files
+            - Automatic expansions of links with shortcuts of shell folders
+            - History of locations only in addition to yours' CLI history
+
+        ## Sample link list file (INI format):
+
+            ```PowerShell
+            appd = =$env:APPDATA # PS expression that returns value of env variable
+
+            pfl = =(Get-ChildItem $PROFILE).DirectoryName # PS expressions that returns
+            psh = =(Get-ChildItem $PROFILE).DirectoryName # path string
+
+            pgfl = %ProgramFiles% # Targets can contains common legacy env vars
+            temp = %Temp%         # Env vars evaluates at call time
+
+            # Ordinal link can contain plain target path
+            jmpr     = D:\\JOB\\PowerShell\\Modules\\Jumper\\
+
+            # Label name can contain some non word symbols
+            jmpr/d   = D:\\JOB\\PowerShell\\Modules\\Jumper\\data
+
+            # even blank
+            jmpr d   = D:\\JOB\\PowerShell\\Modules\\Jumper\\data
+
+            # target path can be valid path of any type of providers:
+            reg/win = HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion
+            ```
+    .Example
+
+        Jump to home (User Profile) dir:
+
+            PS> ~
+
+    .Example
+
+        Jump by label:
+
+            PS> ~ tmp
+
+    .Example
+
+        Whith additional part of path Jumper returns whole path combined of link
+        target and additional part.
+
+        Get link target:
+
+            PS> ~ temp .
+            C:\Temp
+
+            PS> ~ appd nuget
+            C:\Users\SynCap\AppData\Roaming\nuget
+
+    .Example
+
+        To get path without jump to target use `-ToString` switch or its alias - `-s`.
+
+            PS> ~ temp -s
+            C:\Temp
+
+    .Example
+
+        Force change location even additional path given (`-Force` or `-f` switch)
+
+            PS: ~ > ~ appd nuget -f
+            PS: C:\Users\SynCap\AppData\Roaming\NuGet >
+
+    .Example
+
+        Use shortcuts in commands and code:
+
+            PS> Get-ChildItem (~ appd .)
+            ...
+            PS> ls
+
+    .Example
+
+        Jump to system's "Start Menu"
+
+            PS> ~ startMenu
+            ~\AppData\Roaming\Microsoft\Windows\Start Menu
+
 #>
 
 ############################# CodeAnalysis
