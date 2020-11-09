57465886ce2440a270ccc57cf5af6a81bf591d33
SynCap <syncap@ya.ru>
2020-11-09 10:26:35 +0800

    fmt: command centre

--- Jumper.psm1
+++ Jumper.psm1
@@ -420,20 +420,20 @@ function Invoke-JumperCommand {
 
     switch ($Command) {
 
-        { $_ -in ('a', 'add')      } { Add-Jumper @Params;         break }
-        { $_ -in ('c', 'clear')    } { Clear-Jumper;               break }
-        { $_ -in ('d', 'disable')  } { Disable-JumperLink @Params; break }
-        { $_ -in ('e', 'expand')   } { Expand-JumperLink @Params;  break }
-        { $_ -in ('g', 'get')      } { Get-Jumper @Params;         break }
+        { $_ -in ('a',  'add')     } { Add-Jumper @Params;         break }
+        { $_ -in ('c',  'clear')   } { Clear-Jumper;               break }
+        { $_ -in ('d',  'disable') } { Disable-JumperLink @Params; break }
+        { $_ -in ('e',  'expand')  } { Expand-JumperLink @Params;  break }
+        { $_ -in ('g',  'get')     } { Get-Jumper @Params;         break }
         { $_ -in ('rd', 'read')    } { Read-JumperFile @Params;    break }
         { $_ -in ('rt', 'restart') } { Restart-JumperModule;       break }
         { $_ -in ('rv', 'resolve') } { Resolve-JumperList;         break }
-        { $_ -in ('s', 'set')      } { Set-JumperLink @Params;     break }
-        { $_ -in ('sh, history')   } { Show-JumperHistory @Params; break }
+        { $_ -in ('s',  'set')     } { Set-JumperLink @Params;     break }
+        { $_ -in ('sh', 'history') } { Show-JumperHistory @Params; break }
         { $_ -in ('sv', 'save')    } { Save-JumperList @Params;    break }
 
         { $_ -in ('h', 'Help') } {
-            help (Split-Path (g . '.\Jumper.psm1') -LeafBase); break
+        help (Split-Path (g . '.\Jumper.psm1') -LeafBase); break
         }
 
         default {
