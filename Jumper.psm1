261d2c23e00204ac39233bb8361f2d6cb45aa713
Constantin Losk <SynCap@users.noreply.github.com>
2021-01-12 20:05:43 +0800

    fix(use-jumper): use Get-ShellPredefinedFolder but not direct check

--- Jumper.psm1
+++ Jumper.psm1
@@ -530,7 +530,7 @@ function Use-Jumper {
             break;
         }
 
-        { ($Label -in [System.Environment+SpecialFolder].GetEnumNames()) -and (Test-Path ($TestPath = [Environment]::GetFolderPath($Label)))} {
+        { Test-Path (Get-ShellPredefinedFolder $Label) } {
             $Target = $TestPath
             $JumpMessage = "${RC} Label `e[33m", $Label, "${RC} is present.",
             "Found shell folder for it: `e[33m", $Target, $RC -join ''
